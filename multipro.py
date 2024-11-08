ab204700dfd6e9fbdafe0297c010f87488cb65c9
Abhishek <abhishek.udiya@videonetics.com>
2024-11-08 19:28:10 +0530

    updated the code

--- multipro.py
+++ multipro.py
@@ -7,13 +7,13 @@ class Multiprocessing:
 
     def calculation(self, n):
         for i in range(n):
-            job_counter += 1
+            self.job_counter += 1
             print(self.job_counter)
 
 
 cp = Multiprocessing()
-p1 = mp.Process(target=cp.calculation, args=(100,))
-p2 = mp.Process(target=cp.calculation, args=(100,))
+p1 = mp.Process(target=cp.calculation, args=(10,))
+p2 = mp.Process(target=cp.calculation, args=(70,))
 p1.start()
 p2.start()
 p1.join()
